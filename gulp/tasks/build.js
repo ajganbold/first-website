9a65252d616e50705f52aeea821e33502c0415b5
jaga <jagawebdev@gmail.com>
2017-02-28 17:29:18 +0000

    docs folder added and build task created

---
+++ gulp/tasks/build.js
@@ -0,0 +1,36 @@
+var gulp = require("gulp");
+var imagemin = require("gulp-imagemin");
+var del = require("del");
+var usemin = require("gulp-usemin");
+var cssmin = require("gulp-cssmin");
+var rev = require("gulp-rev");
+var uglify = require("gulp-uglify");
+
+gulp.task("deleteDistFolder", function(){
+  return del("./docs");
+});
+
+gulp.task("optimizeImages", ["deleteDistFolder"], function() {
+  return gulp.src(["./app/assets/images/**/*"])
+  .pipe(imagemin({
+    progressive: true,
+    interlaced: true,
+    multipass: true
+  }))
+  .pipe(gulp.dest("./docs/assets/images"));
+});
+
+gulp.task("useminTrigger", ["deleteDistFolder"], function() {
+  gulp.start("usemin");
+});
+
+gulp.task("usemin", ["styles"], function(){
+  return gulp.src("./app/index.html")
+  .pipe(usemin({
+    css: [function() {return rev()}, function() {return cssmin()}],
+    js: [function() {return rev()}, function() {return uglify()}]
+  }))
+  .pipe(gulp.dest("./docs"));
+});
+
+gulp.task("build", ["deleteDistFolder", "optimizeImages", "useminTrigger"]);
